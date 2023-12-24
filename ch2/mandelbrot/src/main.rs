c14fff386e9a8dfa32da4e3ef016d39a689b1785
yangsh <839651726@qq.com>
2023-12-24 17:08:38 +0800

    mandel serial

--- ch2/mandelbrot/src/main.rs
+++ ch2/mandelbrot/src/main.rs
@@ -1,13 +1,40 @@
 use num::Complex;
 use std::str::FromStr;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 5 {
+        eprintln!("Usage:{} FILE PIXELS UPPERLEFT LOWERRIGHT",args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2],'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+    let mut pixels = vec![0;bounds.0*bounds.1];
+
+    render(&mut pixels,bounds,upper_left,lower_right);
+    write_image(&args[1],&pixels,bounds).expect("error writing PNG files");
+
 }
+
 
 //使用Rust特有语法loop无限循环
 fn suqare_loop(mut x:f64){
     loop{
         x= x*x;
+    }
+}
+
+fn complex_suqare_add_loop(c: Complex<f64>){
+    let mut z = Complex{re:0.0, im:0.0};
+    loop{
+        z = z*z+c;
     }
 }
 
@@ -31,6 +58,56 @@ fn parse_pair<T:FromStr>(s:&str, separator:char)->Option<(T,T)>{
 
 }
 
+///把一对用逗号分割的浮点数解析为复数
+fn parse_complex(s:&str)->Option<Complex<f64>>{
+    match parse_pair(s,','){
+        Some((re,im))=>Some(Complex{re,im}),
+        None=>None
+    }
+}
+
+///从像素到复数的映射，绘制出图像中像素点行和列，返回复平面中对应的坐标
+///'bounds' 是一个‘pair’，给出了图像高度宽度。‘pixel’ 是像素坐标； ‘upper_left’ and 'lower_right'是复平面中表示指定图像覆盖范围的点
+fn pixel_to_point(
+                bounds:(usize,usize),
+                pixel:(usize,usize),
+                upper_left:Complex<f64>,
+                lower_right:Complex<f64>)->Complex<f64> {
+    let (width,height) = (lower_right.re - upper_left.re,
+                            upper_left.im - lower_right.im);
+    Complex{
+        re:upper_left.re+pixel.0 as f64 * width / bounds.0 as f64,
+        im:upper_left.im+pixel.1 as f64 * height / bounds.1 as f64
+    //减法是因为在屏幕坐标系中pixel.1是向下递增，但复数虚部是向上递增
+    }
+
+}
+
+///绘制曼德博集
+fn render(pixels: &mut [u8],
+        bounds:(usize,usize),
+        upper_left:Complex<f64>,
+        lower_right:Complex<f64>){
+    assert!(pixels.len() == bounds.1*bounds.0);
+    for row in 0..bounds.1{
+        for column in 0..bounds.0{
+            let point = pixel_to_point(bounds,(column,row),upper_left,lower_right);
+            pixels[row*bounds.0 + column] = match escape_time(point,255){
+                None => 0,
+                Some(count) =>255-count as u8
+            };
+        }
+    }
+    }
+
+///把‘pixels’缓冲区写入文件‘filename’
+fn write_image(filename: &str, pixels :&[u8], bounds:(usize,usize))->Result<(),std::io::Error>{
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels,bounds.0 as u32,bounds.1 as u32, ColorType::Gray(8))?;
+    Ok(())
+}
+
 ///尝试测定'c'是否位于曼德博集中，使用最多'limit'次迭代来判定
 /// 
 /// 如果'c'不是集合成员之一,则返回'some(i)', 其中的'i'是'c'离开以原点
@@ -47,27 +124,32 @@ fn escape_time(c:Complex<f64>,limit:usize)->Option<usize> {
     None
 }
 
-fn complex_suqare_add_loop(c: Complex<f64>){
-    let mut z = Complex{re:0.0, im:0.0};
-    loop{
-        z = z*z+c;
-    }
-}
 
-struct Complex<T>{
-    ///复数的实部
-    re: T,
-    /// 复数的虚部
-    im: T,
-}
+
 
 #[test]
 fn test_parse_pair(){
     assert_eq!(parse_pair::<i32>("",        ','),None);
     assert_eq!(parse_pair::<i32>("10",        ','),None);
     assert_eq!(parse_pair::<i32>(",10",        ','),None);
-    assert_eq!(parse_pair::<i32>("10,20",        ','),Some((10,20));
+    assert_eq!(parse_pair::<i32>("10,20",        ','),Some((10,20)));
     assert_eq!(parse_pair::<i32>("10,20xy",        ','),None);
     assert_eq!(parse_pair::<f64>("0.5x",        'x'),None);
-    assert_eq!(parse_pair::<f64>("0.5x1.5",        'x'),None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5",        'x'),Some((0.5,1.5)));
+}
+
+
+#[test]
+fn test_parse_complex(){
+    assert_eq!(parse_complex("1.24,-4.12"),Some(Complex{re: 1.24, im:-4.12}));
+    assert_eq!(parse_complex(",-4.12"),None);
+    
+}
+
+#[test]
+fn text_pixel_to_point() {
+    assert_eq!(pixel_to_point((100,200),(25,175),
+                            Complex{re:-1.0,im:1.0},
+                            Complex{re:1.0,im:-1.0}),
+                Complex{re:-0.5,im:-0.75})
 }
